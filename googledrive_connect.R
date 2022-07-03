98712084a6068cec2ead8f162f4da44cff57a605
leedozed <leedozed@gmail.com>
2022-07-03 11:38:27 +0900

    2

--- googledrive_connect.R
+++ googledrive_connect.R
@@ -10,3 +10,8 @@ drive_find(type='xlsx')
 df = drive_find(type='xlsx')
 drive_download(file = df$name[3], path = df$name[3], overwrite = TRUE)
 #출처: https://seong6496.tistory.com/262?category=1053822 [파이프마임:티스토리]
+
+drive_upload('D:/Down/millie-app-1.3.0.6.apk',
+              # path = 'https://drive.google.com/drive/folders/10IH5MfylzG8FxcghULziNmvcl0DR3ey6?usp=sharing',
+              name = 'millie-app-1.3.0.6.apk'
+              )
